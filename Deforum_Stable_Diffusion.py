b75dc2a973b6d66481ca953539b8564c22ebd7b0
Yiğithan Yücedağ <yigithanyucedag@gmail.com>
2023-06-16 20:33:42 +0300

    refactor: update

--- Deforum_Stable_Diffusion.py
+++ Deforum_Stable_Diffusion.py
@@ -231,7 +231,7 @@ prompts = {
 }
 
 neg_prompts = {
-    0: "nude, blank, negative space, space, blur, blurry, out of focus, low quality"
+    0: "nude, blank, negative space, space, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands,poorly drawn eyes, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature"
 }
 
 # can be a string, list, or dictionary
